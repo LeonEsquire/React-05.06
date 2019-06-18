ffbb5f068286d43ef666fc004446e9fb952fefca
Leon <leon@MacBook-Leon.local>
2019-06-18 22:37:39 +0300

    Lesson6

---
+++ Lesson5/src/app/components/About_old.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>О нас</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe distinctio nostrum atque numquam quisquam, voluptates voluptatem ratione ut voluptate alias. Laudantium voluptatibus suscipit temporibus voluptatum. Ratione odio sed repudiandae veniam!</p>
+      </div>
+    )
+  }
+}
